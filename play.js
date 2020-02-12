dd1b494efab82205dc24fd61175b180d05c92e99
David <david.he07@yahoo.ca>
2020-02-12 14:08:12 -0500

    completed file play.js

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require("./client");
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput => {
-    if (handleUserInput === "\u0003") {
-      process.exit();
-    }
-  });
-
-  return stdin;
-};
-
-setupInput();
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
