6ce1215c83312dd6a09fe0285e6b08a7caf6c119
David <david.he07@yahoo.ca>
2020-02-12 14:59:40 -0500

    completed input.js

--- input.js
+++ input.js
@@ -1,12 +1,36 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on("data", handleUserInput => {
-    if (handleUserInput === "\u0003") {
+  stdin.on("data", key => {
+    if (key === "\u0003") {
       process.exit();
+    }
+
+    if (key === "w") {
+      connection.write("Move: up");
+      console.log("up");
+    }
+
+    if (key === "s") {
+      connection.write("Move: down");
+      console.log("down");
+    }
+
+    if (key === "a") {
+      connection.write("Move: left");
+      console.log("left");
+    }
+
+    if (key === "d") {
+      connection.write("Move: right");
+      console.log("right");
     }
   });
 
